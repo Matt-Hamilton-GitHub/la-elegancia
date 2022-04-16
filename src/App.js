90f0d6cec5eb20599d17c560c51499f63d5bab51
Matt <i.w.b.s.bilm@gmail.com>
2022-04-15 21:39:03 -0700

    product page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+
 import Home from "./pages/Home";
-import ProductList from './pages/ProductList'
+import Product from './pages/Product'
 
 
 function App() {
   return (
     <div className="App">
-     <ProductList />
+     <Product />
     </div>
   );
 }
