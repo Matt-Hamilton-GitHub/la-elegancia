cce1d901fb92417ea1068ee52cc4a3791ed4da93
Matt <i.w.b.s.bilm@gmail.com>
2022-06-18 17:04:13 -0700

    routes created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import Home from "./pages/Home";
 import Product from './pages/Product'
 import Login from "./pages/Login";
@@ -10,12 +10,20 @@ import Pay from "./components/Pay";
 
 
 function App() {
-  return (
-    <div className="App">
-     <Cart/>
-     <Pay/>
 
-    </div>
+  const user = true;
+
+  return (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} exact />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        {/* {user ? elemetnt=<Navigate to='/' replace/> : <Route path="/login" element={<Login />} />} */}
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/register" element={<Register />} />
+    </Routes>  
+  </Router>
   );
 }
 
